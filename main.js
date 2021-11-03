c03b6cc99b55ffb24e0c623ca336123369dd371e
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2021-11-03 19:39:06 -0300

    Add ScoreBoad in main

--- main.js
+++ main.js
@@ -2,16 +2,15 @@ import './src/styles/settings/colors.css';
 import './src/styles/generics/reset.css'
 import './src/styles/elements/base.css';
 import BoardGame from './src/objects/BoardGame';
-import PlayerName from './src/components/PlayerName';
+import ScoreBoard from './src/objects/ScoreBoard';
 
 const root = document.querySelector('#root');
 
 
 root.insertAdjacentHTML('beforeend', 
   `
-  ${PlayerName('Player1')}
-  ${PlayerName('Player2')}
-  ${BoardGame(6)}
+    ${ScoreBoard()}
+    ${BoardGame(6)}
   `
 
 );
